228c4159f2bf6cd8814c8c3ad214fdb2a6bf221a
PavelDenisov <pavel.den111@gmail.com>
2023-02-22 09:56:59 +0200

    done

--- src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
@@ -2,5 +2,22 @@ package school.mjc.stage0.conditions.task4;
 
 public class MaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int max = first;
+
+        if (first < second) {
+            max = second;
+        }
+        else if (first < third) {
+            max = third;
+        }
+        else {
+            System.out.println(first);
+        }
+
     }
 }
+//1. Greatest from 3.
+//        Implement the program that will consume 3 numbers from method arguments
+//        and it will print the print greatest out of three.
+//        In case if there are more than 1 biggest -> print any of them.(Libraries are forbidden)
+
--- src/main/java/school/mjc/stage0/conditions/task4/TernaryGreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryGreatestNumberPrinter.java
@@ -2,5 +2,17 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryGreatestNumberPrinter {
     public void printGreatest(int first, int second) {
+        int max = first;
+        int greatest = (first > second) ? first : second;
+        }
     }
-}
+
+//1. Greatest number printer.
+//        Implement the program that will find the greatest number
+//        from two that are passed as parameters to the method and print the greatest.
+//        In case of equality print any of them. Implement program in the code snippet:
+//
+//public class GreatestNumberPrinter {
+//    public void printGreatest(int first, int second) {
+//    }
+//}
